820e143f5ded9d0b06717fb92c1484664e553244
elkobia <eslam.m.baher@gmail.com>
2023-12-04 16:16:38 +1100

    updated fetch_config_from_yaml to handle None cases

--- section-07-ci-and-publishing/model-package/regression_model/config/core.py
+++ section-07-ci-and-publishing/model-package/regression_model/config/core.py
@@ -72,7 +72,7 @@ def find_config_file() -> Path:
 def fetch_config_from_yaml(cfg_path: Path = None) -> YAML:
     """Parse YAML containing the package configuration."""
 
-    if not cfg_path:
+    if cfg_path is None:
         cfg_path = find_config_file()
 
     if cfg_path:
